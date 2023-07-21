97f48c5bb457d9decde38bb02dc8aa93a343bca4
AarushMohapatra <103845656+AarushMohapatra@users.noreply.github.com>
2023-07-21 18:57:35 -0400

    i removed a hastag :D

--- room.js
+++ room.js
@@ -35,7 +35,7 @@ function getData() {
       childKey = childSnapshot.key;
       Room_names = childKey;
       console.log("Room Name - " + Room_names);
-      row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+      row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)' >" + Room_names + "</div><hr>";
       document.getElementById("output").innerHTML += row;
     });
   });
