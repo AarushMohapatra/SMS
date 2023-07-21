e37b4d0d8e9dc51098c41ab8dd27447f02068104
AarushMohapatra <103845656+AarushMohapatra@users.noreply.github.com>
2023-07-21 19:09:54 -0400

    like removal/extermination

--- page.js
+++ page.js
@@ -21,7 +21,7 @@ function send()
   firebase.database().ref("rooms/"+room_name).push({
     name:user_name,
     message:msg,
-    like:0
+    //like:0
    });
 
   document.getElementById("msg").value = "";
@@ -35,19 +35,19 @@ function getData() { firebase.database().ref("rooms/"+room_name).on('value', fun
 	       console.log(message_data);
 	       name = message_data['name'];
 	       message = message_data['message'];
-         like = message_data['like'];
+         //like = message_data['like'];
          name_with_tag = "<h4> "+ name +"<img class='user_tick' src='tick.png'></h4>";
          message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
 like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
-         span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
+         //span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
 
-        row = name_with_tag + message_with_tag +like_button + span_with_tag;       
+        row = name_with_tag + message_with_tag +like_button// + span_with_tag;       
         document.getElementById("output").innerHTML += row;
 //End code
       } });  }); }
 getData();
 
-function updateLike(message_id)
+/*function updateLike(message_id)
 {
   console.log("clicked on like button - " + message_id);
 	button_id = message_id;
@@ -59,7 +59,7 @@ function updateLike(message_id)
 		like : updated_likes  
 	 });
 
-}
+}*/
 
 function logout() {
 localStorage.removeItem("user_name");
